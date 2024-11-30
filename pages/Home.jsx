f5c9f9d7a0cac9105f28b67496cec150cb524547
mxgdt <113356145+mxgdt@users.noreply.github.com>
2024-11-30 17:04:26 +0300

    Update Home.jsx

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -9,28 +9,3 @@ const Home = () => (
 );
 
 export default Home;
-
-// src/pages/RatesPage.jsx
-import React, { useState } from "react";
-import Rates from "../components/Rates";
-
-const RatesPage = () => {
-  const [baseCurrency, setBaseCurrency] = useState("USD");
-
-  return (
-    <div>
-      <h1>Курсы валют</h1>
-      <select
-        value={baseCurrency}
-        onChange={(e) => setBaseCurrency(e.target.value)}
-      >
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-        <option value="RUB">RUB</option>
-      </select>
-      <Rates baseCurrency={baseCurrency} />
-    </div>
-  );
-};
-
-export default RatesPage;
