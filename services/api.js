be803609d16a44fcdc2fd1b1e80a8bf169426a14
mxgdt <113356145+mxgdt@users.noreply.github.com>
2024-11-30 16:59:21 +0300

    Update api.js

--- services/api.js
+++ services/api.js
@@ -1,1 +1,15 @@
+const BASE_URL = "https://api.exchangerate.host";
 
+export const getRates = async (baseCurrency) => {
+  const response = await fetch(`${BASE_URL}/latest?base=${baseCurrency}`);
+  const data = await response.json();
+  return data.rates;
+};
+
+export const convertCurrency = async (amount, from, to) => {
+  const response = await fetch(
+    `${BASE_URL}/convert?from=${from}&to=${to}&amount=${amount}`
+  );
+  const data = await response.json();
+  return data.result;
+};
