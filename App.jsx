f2469b8a560d8ae216fadbee86f81d8832e12151
mxgdt <113356145+mxgdt@users.noreply.github.com>
2024-11-30 17:00:51 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import RatesPage from "./pages/RatesPage";
 
+const App = () => (
+  <Router>
+    <nav>
+      <Link to="/">Конвертер</Link> | <Link to="/rates">Курсы валют</Link>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/rates" element={<RatesPage />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
