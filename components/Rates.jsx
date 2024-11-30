3984748ea6f68915556f11da0f0b5d4df349a3be
mxgdt <113356145+mxgdt@users.noreply.github.com>
2024-11-30 17:00:11 +0300

    Update Rates.jsx

--- components/Rates.jsx
+++ components/Rates.jsx
@@ -1,1 +1,33 @@
+import React, { useEffect, useState } from "react";
+import { getRates } from "../services/api";
 
+const Rates = ({ baseCurrency }) => {
+  const [rates, setRates] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchRates = async () => {
+      const fetchedRates = await getRates(baseCurrency);
+      setRates(fetchedRates);
+      setLoading(false);
+    };
+    fetchRates();
+  }, [baseCurrency]);
+
+  if (loading) return <p>Загрузка курсов...</p>;
+
+  return (
+    <div>
+      <h2>Текущие курсы валют (Базовая: {baseCurrency})</h2>
+      <ul>
+        {Object.entries(rates).map(([currency, rate]) => (
+          <li key={currency}>
+            1 {baseCurrency} = {rate} {currency}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Rates;
